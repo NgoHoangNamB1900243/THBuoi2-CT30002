12064f1912d640d97eb82b4becf7dd2491c68690
Ngo Hoang Nam <namb1900243@student.ctu.edu.vn>
2023-02-22 17:21:32 +0700

    vong lap for

--- VongLapFor.java
+++ VongLapFor.java
@@ -1,6 +1,6 @@
 public class VongLapFor {
     public static void main(String[] args) {
-        // in các số từ 1 đến 1000
+        // in các số từ 1 đến 10
         for(int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
